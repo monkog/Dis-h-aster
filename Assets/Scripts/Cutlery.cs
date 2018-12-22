d327c5fc22bfb3b44ffbb98a6da17fa982ddc190
monkog <monkog@wp.pl>
2018-12-22 22:20:44 +0100

    Cutlery spawning and moving objects

---
+++ Assets/Scripts/Cutlery.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Cutlery : MonoBehaviour
+{
+	private const string FloorTag = "Floor";
+
+	private const float Speed = 0.1f;
+
+	private bool _canMove;
+
+	// Use this for initialization
+	void Start()
+	{
+		_canMove = true;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (!_canMove) return;
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+			transform.Translate(-Speed, 0, 0);
+		if (Input.GetKey(KeyCode.RightArrow))
+			transform.Translate(Speed, 0, 0);
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == FloorTag)
+			Destroy(gameObject);
+
+		_canMove = false;
+	}
+}
---
+++ Assets/Scripts/CutlerySpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CutlerySpawner : MonoBehaviour
+{
+	public GameObject[] CutleryPrefabs;
+	private const float Delay = 4f;
+	private int _cutleryCount;
+
+	// Use this for initialization
+	void Start()
+	{
+		StartCoroutine(SpawnCutleryCoroutine());
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	private IEnumerator SpawnCutleryCoroutine()
+	{
+		while (true)
+		{
+			_cutleryCount++;
+			var position = new Vector3(Random.Range(-0.6f, 0.85f), transform.position.y, 0);
+			var cutlery = Instantiate(CutleryPrefabs[Random.Range(0, CutleryPrefabs.Length)], position, Quaternion.identity);
+			cutlery.GetComponent<SpriteRenderer>().sortingOrder = _cutleryCount;
+			yield return new WaitForSeconds(Delay);
+		}
+	}
+}
