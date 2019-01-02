c58d54b3dd3b57a4ab13ea1beac58c43ff5c8dd3
monkog <monkog@wp.pl>
2019-01-02 20:24:34 +0100

    Spawn bonus in random position

--- Assets/Scripts/BonusSpawner.cs
+++ Assets/Scripts/BonusSpawner.cs
@@ -6,7 +6,9 @@ namespace Assets.Scripts
 {
 	public class BonusSpawner : MonoBehaviour
 	{
-		private const float Timeout = 2;
+		private const float Timeout = 5;
+
+		public GameObject Camera;
 		public GameObject[] BonusPrefabs;
 
 		void Start()
@@ -16,11 +18,17 @@ namespace Assets.Scripts
 
 		private IEnumerator GenerateBonus()
 		{
-			var random = Random.Range(0, 5);
-			if (random != 0 || GetComponents<Bonus>().Any()) yield return new WaitForSeconds(Timeout);
+			while (!GameLogic.Instance.IsGameOver)
+			{
+				var random = Random.Range(0, 10);
+				if (random != 0 || GetComponents<Bonus>().Any()) yield return new WaitForSeconds(Timeout);
 
-			Instantiate(BonusPrefabs[Random.Range(0, BonusPrefabs.Length)]);
-			yield return new WaitForSeconds(Timeout);
+				var bonus = BonusPrefabs[Random.Range(0, BonusPrefabs.Length)];
+				var x = Random.Range(-5.0f, 5.0f);
+				var y = Camera.transform.position.y + Random.Range(1.5f, 4.0f);
+				Instantiate(bonus, new Vector3(x, y), Quaternion.identity);
+				yield return new WaitForSeconds(Timeout);
+			}
 		}
 	}
 }
